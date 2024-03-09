68593d0ff8bf81aa29504448081e270138ae0f68
miruaraya <144387687+miruaraya@users.noreply.github.com>
2024-03-09 14:54:46 -0500

    Create register.js

---
+++ bin/register.js
@@ -0,0 +1,14 @@
+ 
+function GEEKFORGEEKS() { 
+    const Fname = 
+        document.forms.RegForm.fname.value;
+    const Lname =
+        document.forms.RegForm.lname.value;
+    const Email = 
+        document.forms.RegForm.email.value; 
+    const Pass = 
+        document.forms.RegForm.pass.value;
+    const Cpass = 
+        document.forms.RegForm.cpass.value;
+  
+    console.log(Fname, Lname, Email, Pass); 
