24228b649dd7d6ff036073828d2c9caf31330182
miruaraya <144387687+miruaraya@users.noreply.github.com>
2024-03-11 11:15:01 -0400

    Update register.js

--- bin/register.js
+++ bin/register.js
@@ -1,96 +1,66 @@
-const form = document.querySelector('#form')
-const fname = document.querySelector('#fname');
-const lname = document.querySelector('#lname');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const cpassword = document.querySelector('#cpassword');
+function createAccount(){
+    form.addEventListener('submit', (e)=>{
+        const form = document.getElementByID('form');
+        const fname = document.getElementByID('fname');
+        const lname = document.getElementByID('lname');
+        const email = document.getElementByID('email');
+        const pass = document.getElementByID('pass');
+        const cpass = document.getElementByID('cpass');
 
-form.addEventListener('submit',(e)=>{
-    
-    if(!validateInputs()){
-        e.preventDefault();
-        alert("Unsuccessful Registration");
-    }
-})
+        if(!validateInputs()){
+            alert("Registration Unsuccessful");
+            return false;
+        }
+        else{
+            alert("Registration Successful");
+            const fs = require('fs');
+            let csvContent = fname + "," + lname + "," + email + "," + password + "," + "\r\n";
+            fs.writeFile("data.csv", csvContent, (err) => {
+                if (err) throw err;
+            })
+            return true;
+        }
+    })
+}
 
 function validateInputs(){
     const fnameVal = fname.value.trim()
     const lnameVal = lname.value.trim()
     const emailVal = email.value.trim();
-    const passwordVal = password.value.trim();
-    const cpasswordVal = cpassword.value.trim();
+    const passwordVal = pass.value.trim();
+    const cpasswordVal = cpass.value.trim();
     let success = true
 
     if(fnameVal===''){
         success=false;
-        setError(fname,'First and last name is required')
+        setError(username,'Name is required')
     }
     else{
         setSuccess(fname)
-    }
-
-    if(emailVal===''){
-        success = false;
-        setError(email,'Email is required')
-    }
-    else if(!validateEmail(emailVal)){
-        success = false;
-        setError(email,'Please enter a valid email')
-    }
-    else{
-        setSuccess(email)
-    }
+	@@ -53,26 +42,26 @@ function validateInputs(){
 
     if(passwordVal === ''){
         success= false;
-        setError(password,'Password is required')
+        setError(pass,'Password is required')
     }
     else if(passwordVal.length<8){
         success = false;
-        setError(password,'Password must be atleast 8 characters long')
+        setError(pass,'Password must be atleast 8 characters long')
     }
     else{
-        setSuccess(password)
+        setSuccess(pass)
     }
 
     if(cpasswordVal === ''){
         success = false;
-        setError(cpassword,'Confirm password is required')
+        setError(cpass,'Confirm password is required')
     }
     else if(cpasswordVal!==passwordVal){
         success = false;
-        setError(cpassword,'Password does not match')
+        setError(cpass,'Password does not match')
     }
     else{
-        setSuccess(cpassword)
+        setSuccess(cpass)
     }
 
     return success;
-
-}
-//element - password, msg- pwd is reqd
-function setError(element,message){
-    const inputGroup = element.parentElement;
-    const errorElement = inputGroup.querySelector('.error')
-
-    errorElement.innerText = message;
-    inputGroup.classList.add('error')
-    inputGroup.classList.remove('success')
-}
-
-function setSuccess(element){
-    const inputGroup = element.parentElement;
-    const errorElement = inputGroup.querySelector('.error')
-
-    errorElement.innerText = '';
-    inputGroup.classList.add('success')
-    inputGroup.classList.remove('error')
-}
-
-const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
