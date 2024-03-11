7f1618683aa1c13fd4db48518f97a0aa3a80f0b6
miruaraya <144387687+miruaraya@users.noreply.github.com>
2024-03-10 20:57:48 -0400

    Update register.js

--- bin/register.js
+++ bin/register.js
@@ -1,28 +1,94 @@
- 
-function RegisterForm() { 
-    const Fname = 
-        document.forms.RegForm.fname.value;
-    const Lname =
-        document.forms.RegForm.lname.value;
-    const Email = 
-        document.forms.RegForm.email.value; 
-    const Pass = 
-        document.forms.RegForm.pass.value;
-    const Cpass = 
-        document.forms.RegForm.cpass.value;
-  
-    console.log(Fname, Lname, Email, Pass); 
-    if (pass.length < 6) { 
-        alert 
-            ("Password should be atleast 6 character long"); 
-        password.focus(); 
-        return false; 
-    } 
-    if (!(pass == cpass)) {
-        alert
-          ("Passwords should be the same");
-        return false;
+const form = document.querySelector('#form')
+const username = document.querySelector('#fname');
+const username = document.querySelector('#lname');
+const email = document.querySelector('#email');
+const password = document.querySelector('#pass');
+const cpassword = document.querySelector('#cpass');
+
+form.addEventListener('submit', (e)=>{
+    if(!validateInputs()){
+        e.preventDefault();
     }
- 
-    return true;
+})
+                      
+function validateInputs(){
+    const fnameVal = fname.value.trim()
+    const lnameVal = lname.value.trim()
+    const emailVal = email.value.trim();
+    const passwordVal = pass.value.trim();
+    const cpasswordVal = cpass.value.trim();
+    let success = true
+
+    if(fnameVal===''){
+        success=false;
+        setError(username,'Name is required')
+    }
+    else{
+        setSuccess(fname)
+    }
+
+    if(emailVal===''){
+        success = false;
+        setError(email,'Email is required')
+    }
+    else if(!validateEmail(emailVal)){
+        success = false;
+        setError(email,'Please enter a valid email')
+    }
+    else{
+        setSuccess(email)
+    }
+
+    if(passwordVal === ''){
+        success= false;
+        setError(pass,'Password is required')
+    }
+    else if(passwordVal.length<8){
+        success = false;
+        setError(pass,'Password must be atleast 8 characters long')
+    }
+    else{
+        setSuccess(pass)
+    }
+
+    if(cpasswordVal === ''){
+        success = false;
+        setError(cpass,'Confirm password is required')
+    }
+    else if(cpasswordVal!==passwordVal){
+        success = false;
+        setError(cpass,'Password does not match')
+    }
+    else{
+        setSuccess(cpass)
+    }
+
+    return success;
+
 }
+//element - password, msg- pwd is reqd
+function setError(element,message){
+    const inputGroup = element.parentElement;
+    const errorElement = inputGroup.querySelector('.error')
+
+    errorElement.innerText = message;
+    inputGroup.classList.add('error')
+    inputGroup.classList.remove('success')
+}
+
+function setSuccess(element){
+    const inputGroup = element.parentElement;
+    const errorElement = inputGroup.querySelector('.error')
+
+    errorElement.innerText = '';
+    inputGroup.classList.add('success')
+    inputGroup.classList.remove('error')
+}
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
